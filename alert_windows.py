9c93b969317c88601ca47a76991b4cf718367ff7
M-Slastukhin <100306561+M-Slastukhin@users.noreply.github.com>
2022-05-16 14:41:07 +0400

    добавлено руководство пользователя

--- alert_windows.py
+++ alert_windows.py
@@ -4,10 +4,12 @@ import PySimpleGUI as sg
 def no_landusers_list():
     sg.theme('BluePurple')
     layout = [[sg.Text('Отсутствует фаил landusers_list.xlsx')],
-              [sg.Button('Выход')]]
+              [sg.Button('Руководство пользователя'), sg.Button('Выход')]]
     window = sg.Window('Заполнение писем', layout, element_justification='c')
     while True:
         event, values = window.read()
+        if event == 'Руководство пользователя':
+            user_guide()
         if event == sg.WIN_CLOSED or event == 'Выход':  # закрытие окна
             break
     window.close()
@@ -23,3 +25,26 @@ def no_act():
 
 def no_plan():
     sg.popup('Отсутствует шаблон схемы', 'Добавьте документ "схема_шаблон.vsdx" в папку с программой')
+
+
+def user_guide():
+    sg.popup('Для использования программы необходимо разместить подговотовленные файлы '
+             'landusers_list.xlsx, письмо_шаблон.docx, акт_шаблон.docx, схема_шаблон.vsdx '
+             'в папке с программой.\n\nФаил landusers_list должен содержать исходные данные по '
+             'категориям:\n  название организации (organization),\n  должность (position),\n  фамилия (surname),\n '
+             ' имя (name),\n  отчество (patronymic),\n  пол (gender),\n  адрес (adress),\n  '
+             'тип(землепользователь/строительная организация) (type)\n\nПодстановка данных в письмо,'
+             ' акт или схему осуществляется заменой ключевых слов.\n\nДоступные ключевые слова:\n'
+             '{{organization}} - название организации\n'
+             '{{organization_r}} - название организации в родительном падеже\n'
+             '{{position}} - должность\n'
+             '{{position_d}} - должность в дательном падеже\n'
+             '{{name}} - имя\n'
+             '{{patronymic}} - отчество\n'
+             '{{surname}} - фамилия\n'
+             '{{surname_d}} - фамилия в дательном падеже\n'
+             '{{initials}} - инициалы\n'
+             '{{accoct}} - обращение (Уважаемый/Уважаемая)\n'
+             '{{adress}} - адрес\n'
+             '{{territory}} - для землепользователей название организации в родительном падеже, для строительных организаций "Саратов и Саратовская область"'
+             )
--- filling-letters.py
+++ filling-letters.py
@@ -1,6 +1,7 @@
 import PySimpleGUI as sg
 from fl_def import choice_data, lander_info
 from fl_doc import mail_save, act_save, plan_save
+from alert_windows import user_guide
 from ContextClass import ContextClass
 
 lander_data = lander_info()  # полчучение информации из exel по-умолчанию для нулевого землепользователя
@@ -11,7 +12,7 @@ sg.theme('BluePurple')   # цвет подложки
 # наполнение страницы. текст, выподающий список и кнопки
 layout = [  [sg.Text('Выбор землепользователя')],
             [sg.InputCombo(choice_data, key='-combo_choice-', enable_events=True)],
-            [sg.Button('Создать письмо'), sg.Button('Создать акт'), sg.Button('Создать схему'), sg.Button('Выход')]]
+            [sg.Button('Создать письмо'), sg.Button('Создать акт'), sg.Button('Создать схему'), sg.Button('Руководство пользователя'), sg.Button('Выход')]]
 
 # создание окна
 window = sg.Window('Заполнение писем', layout)
@@ -30,6 +31,8 @@ while True:
         act_save(info.get())
     if event == 'Создать схему':
         plan_save(info.get())
+    if event == 'Руководство пользователя':
+        user_guide()
     if event == sg.WIN_CLOSED or event == 'Выход':  # закрытие окна
         break
     #print('You entered ', values[0])
